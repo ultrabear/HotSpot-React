c5b3008832233bd8bbe95aab52e1b1aedbee761a
ultrabear <bearodark@gmail.com>
2024-09-19 16:47:58 -0700

    add new user create

--- backend/routes/api/users.ts
+++ backend/routes/api/users.ts
@@ -1,12 +1,32 @@
-import { Router } from 'express';
+import { Router } from "express";
+
+import bcrypt from "bcryptjs";
+
+import { setTokenCookie, requireAuth } from "../../utils/auth.js";
+import { prisma } from "../../dbclient.js";
 
 const router = Router();
 
+router.post("/", async (req, res) => {
+	const { email, password, username } = req.body;
+	const hashedPassword = bcrypt.hashSync(password);
+	const user = await prisma.user.create({
+		data: { email, username, hashedPassword },
+	});
+
+	const safeUser = {
+		id: user.id,
+		email: user.email,
+		username: user.username,
+	};
+
+	setTokenCookie(res, safeUser);
+
+	return res.json({
+		user: safeUser,
+	});
+});
 
 
 
-
-
-
-
-export default router
+export default router;
