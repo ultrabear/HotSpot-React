41298e9c0a98523ca75f31e2afa738b2545e2602
ultrabear <bearodark@gmail.com>
2024-09-23 08:35:21 -0700

    first test spec woo

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -7,16 +7,22 @@ async function main() {
 			{
 				email: "demo@user.io",
 				username: "Demo-lition",
+				firstName: "John",
+				lastName: "Doe",
 				hashedPassword: bcrypt.hashSync("password1"),
 			},
 			{
 				email: "user1@user.io",
 				username: "FakeUser1",
+				firstName: "Jane",
+				lastName: "Doe",
 				hashedPassword: bcrypt.hashSync("password2"),
 			},
 			{
 				email: "user2@user.io",
 				username: "FakeUser2",
+				firstName: "among",
+				lastName: "Us",
 				hashedPassword: bcrypt.hashSync("foopassword"),
 			},
 		],
--- backend/routes/api/users.ts
+++ backend/routes/api/users.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
 import { handleValidationErrors } from "../../utils/validation.js";
 import bcrypt from "bcryptjs";
-import {check} from "express-validator";
+import { check } from "express-validator";
 
 import { setTokenCookie, requireAuth } from "../../utils/auth.js";
 import { prisma } from "../../dbclient.js";
@@ -9,30 +9,33 @@ import { prisma } from "../../dbclient.js";
 const router = Router();
 
 const validateSignup = [
-	check('email')
-	  .exists({ checkFalsy: true })
-	  .isEmail()
-	  .withMessage('Please provide a valid email.'),
-	check('username')
-	  .exists({ checkFalsy: true })
-	  .isLength({ min: 4 })
-	  .withMessage('Please provide a username with at least 4 characters.'),
-	check('username')
-	  .not()
-	  .isEmail()
-	  .withMessage('Username cannot be an email.'),
-	check('password')
-	  .exists({ checkFalsy: true })
-	  .isLength({ min: 6 })
-	  .withMessage('Password must be 6 characters or more.'),
-	handleValidationErrors
-  ];
+	check("email")
+		.exists({ checkFalsy: true })
+		.isEmail()
+		.withMessage("Please provide a valid email."),
+	check("username")
+		.exists({ checkFalsy: true })
+		.isLength({ min: 4 })
+		.withMessage("Please provide a username with at least 4 characters."),
+	check("username").not().isEmail().withMessage("Username cannot be an email."),
+	check("password")
+		.exists({ checkFalsy: true })
+		.isLength({ min: 6 })
+		.withMessage("Password must be 6 characters or more."),
+	check("firstName")
+		.exists({ checkFalsy: true })
+		.withMessage("firstName must be passed"),
+	check("lastName")
+		.exists({ checkFalsy: true })
+		.withMessage("lastName must be passed"),
+	handleValidationErrors,
+];
 
-router.post("/",validateSignup, async (req: Request, res: Response) => {
-	const { email, password, username } = req.body;
+router.post("/", validateSignup, async (req: Request, res: Response) => {
+	const { email, password, username, firstName, lastName } = req.body;
 	const hashedPassword = bcrypt.hashSync(password);
 	const user = await prisma.user.create({
-		data: { email, username, hashedPassword },
+		data: { email, username, hashedPassword, firstName, lastName },
 	});
 
 	const safeUser = {
@@ -44,10 +47,8 @@ router.post("/",validateSignup, async (req: Request, res: Response) => {
 	setTokenCookie(res, safeUser);
 
 	return res.json({
-		user: safeUser,
+		user: { ...safeUser, firstName, lastName },
 	});
 });
-
-
 
 export default router;
