c54f549632171ab606c515d658cbc6736b370f89
ultrabear <bearodark@gmail.com>
2024-09-25 09:16:27 -0700

    get user bookings

---
+++ backend/routes/api/bookings.ts
@@ -0,0 +1,60 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { check } from "express-validator";
+import { handleValidationErrors } from "../../utils/validation.js";
+
+const router = Router();
+
+import bcrypt from "bcryptjs";
+import { setTokenCookie, restoreUser, requireAuth } from "../../utils/auth.js";
+import { prisma } from "../../dbclient.js";
+
+router.get("/current", requireAuth, async (req, res) => {
+	const user = req.user!;
+
+	let bookings = await prisma.booking.findMany({
+		where: { userId: user.id },
+		include: {
+			spot: {
+				include: {
+					images: { where: { preview: true }, select: { url: true } },
+				},
+				select: {
+					id: true,
+					ownerId: true,
+					address: true,
+					city: true,
+					state: true,
+					country: true,
+					lat: true,
+					lng: true,
+					name: true,
+					price: true,
+				},
+			},
+		},
+	});
+
+
+	const sequelized = bookings.map((b) => {
+
+		const { spot, startDate, endDate, ...rest } = b;
+
+		const { images, ...spotRest } = spot;
+
+		return {
+			Spot: {
+				previewImage: images[0]?.url,
+				...spotRest,
+			},
+			startDate: startDate.toDateString(),
+			endDate: endDate.toDateString(),
+			...rest
+		}
+
+	});
+
+	return res.json({Bookings: sequelized});
+
+});
+
+export default router;
