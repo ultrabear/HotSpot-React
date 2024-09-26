212e1a7593163d774dfbd1972b2d6f86505b9373
James Hightower <jamesehightower@gmail.com>
2024-09-26 12:11:12 -0500

    Update bookings.ts
    updated the routes since they are scoped

--- backend/routes/api/bookings.ts
+++ backend/routes/api/bookings.ts
@@ -54,7 +54,7 @@ router.get("/current", requireAuth, async (req, res) => {
 
 // delete booking by  bookingid
 
-const deleteBooking = (url: string, requireAuth: RequireAuth) => {
+router.delete('/:bookingId, requireAuth, async (req, res) => {
 
 	return async (req: Request, res: Response) => {
 	  const { bookingId } = req.params;
