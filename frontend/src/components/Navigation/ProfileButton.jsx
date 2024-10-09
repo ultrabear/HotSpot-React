dfcd8878e42593a408509f9896b572502a579b47
ultrabear <bearodark@gmail.com>
2024-10-09 16:26:59 -0700

    p3 done

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,21 +1,48 @@
 import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
 import { AiOutlineUser } from "react-icons/ai";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 
 function ProfileButton({ user }) {
 	const dispatch = useDispatch();
+	const [showMenu, setShowMenu] = useState(false);
+
+	const ulRef = useRef();
 
 	const logout = (e) => {
 		e.preventDefault();
 		dispatch(sessionActions.logout());
 	};
 
+	const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+	const toggleMenu = (e) => {
+		e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+		setShowMenu(!showMenu);
+	};
+
+	useEffect(() => {
+		if (!showMenu) return;
+
+		const closeMenu = (e) => {
+			if (ulRef.current && !ulRef.current.contains(e.target)) {
+				setShowMenu(false);
+			}
+		};
+
+		document.addEventListener("click", closeMenu);
+
+		return () => document.removeEventListener("click", closeMenu);
+	}, [showMenu]);
+
 	return (
 		<>
-			<button>
+			<button onClick={toggleMenu}>
 				<AiOutlineUser />
 			</button>
-			<ul className="profile-dropdown">
+			<ul className={ulClassName} ref={ulRef}>
 				<li>{user.username}</li>
 				<li>
 					{user.firstName} {user.lastName}
