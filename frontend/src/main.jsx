0c48914039ac223e13fdbe242a7cd435ade33d7a
ultrabear <bearodark@gmail.com>
2024-10-09 14:55:24 -0700

    frontend p0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,17 @@ import configureStore from "./store";
 
 const store = configureStore();
 
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+// ... const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
