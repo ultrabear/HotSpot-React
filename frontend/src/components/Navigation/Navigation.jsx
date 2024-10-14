3df3a20a2742475a5a1c685cebd327532029c7d5
ultrabear <bearodark@gmail.com>
2024-10-14 14:49:02 -0700

    TODO eslint in editor?

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,27 +1,21 @@
 import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
-import * as sessionActions from "../../store/session";
-import OpenModalButton from "../OpenModalButton/OpenModalButton";
-import LoginFormModal from "../LoginFormModal/LoginFormModal";
 import "./Navigation.css";
-import SignupFormModal from "../SignupFormModal/SignupFormModal";
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector((state) => state.session.user);
-	const dispatch = useDispatch();
-
-	const logout = (e) => {
-		e.preventDefault();
-		dispatch(sessionActions.logout());
-	};
 
 	return (
 		<ul>
 			<li>
 				<NavLink to="/">Home</NavLink>
 			</li>
-			{isLoaded && (<li><ProfileButton user={sessionUser}/></li>)}
+			{isLoaded && (
+				<li>
+					<ProfileButton user={sessionUser} />
+				</li>
+			)}
 		</ul>
 	);
 }
